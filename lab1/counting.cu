5e536c696a404482be8bbe2c6e6d8d620cbf61cc
yafangshih <ya.fang.shih.tw@gmail.com>
2016-03-20 18:01:35 +0800

    ver 0.1 serial+parallel prefixadd

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -7,6 +7,13 @@
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
 
+#include "SyncedMemory.h"
+
+/*
+nvcc -std=c++11 -arch=sm_30 -O2 -c counting.cu -o counting.o
+nvcc -std=c++11 -arch=sm_30 -O2 main.cu counting.o -o $(EXE)
+*/
+
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
@@ -29,6 +36,27 @@ __global__ void FindnonText(const char *input_gpu, int *nontxtList_gpu, int fsiz
 	__syncthreads();
 }
 
+__device__ int power2(int pow){
+	int ans = 1;
+	for(int i=0; i<pow; i++){
+		ans = ans * 2;
+	}
+	return ans;
+}
+
+__global__ void indexSum(int *indexList, int len, int donetxt){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	int val = indexList[donetxt + idx]; // range[threadIdx.x];
+	for(int i=0; i<9; i++){
+		if(((int)threadIdx.x - power2(i)) >= 0){
+			val = val + indexList[donetxt + idx - power2(i)];
+			__syncthreads();
+			indexList[donetxt + idx] = val;
+		}
+	}
+}
+
 void CountPosition(const char *text, int *pos, int text_size)
 {
 	/*
@@ -41,6 +69,22 @@ void CountPosition(const char *text, int *pos, int text_size)
 	FindnonText<<<(text_size/32)+1, 32>>>(text, pos, text_size);
 	int cpuList[text_size];
 	cudaMemcpy(cpuList, pos, text_size*sizeof(int), cudaMemcpyDeviceToHost);
+
+	int donetxt = 0, len = 0;
+	int *startptr = cpuList;
+	int *endptr = startptr;
+	while(donetxt < text_size){
+
+		len = 0;
+		while(*startptr != 1){startptr++; donetxt++;}
+		endptr = startptr;
+		while(*endptr == 1){endptr++; len++;}
+
+		indexSum<<<1, len>>>(pos, len, donetxt);
+		startptr = endptr;
+		donetxt = donetxt + len;
+	}
+/**
 	int count=0;
 	for(int i=0;i<text_size;i++){
 		if(cpuList[i]==0){
@@ -50,17 +94,8 @@ void CountPosition(const char *text, int *pos, int text_size)
 			count++;
 			cpuList[i]=count;
 		}
-//	cudaMemcpy(pos, &count, sizeof(int), cudaMemcpyHostToDevice);
-//	pos++;
-	//	printf("%d",count);
-	}
-	cudaMemcpy(pos, cpuList, text_size*sizeof(int), cudaMemcpyHostToDevice);
+*/
 
-	//printf("in counting: %d ",temp);
-	/*for(int i=0;i<text_size;i++){
-		printf("hihi ");
-		//printf("%d ",pos[i]);
-	}*/
 
 }
 
