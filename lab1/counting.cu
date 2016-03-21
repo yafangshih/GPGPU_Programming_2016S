04cc4a576c8de9334a8196cf7e89656bbb48344d
yafangshih <ya.fang.shih.tw@gmail.com>
2016-03-21 13:38:32 +0800

    indextreeSum 1

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -17,6 +17,8 @@ nvcc -std=c++11 -arch=sm_30 -O2 main.cu counting.o -o main
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
 
+__device__ int tree[10][50000000];
+
 __device__ int CheckisText(char input){ // return 1 for english chars
 	if('a'<=input and input<='z'){ return 1; }
 	if('A'<=input and input<='Z'){ return 1; }
@@ -33,7 +35,8 @@ __global__ void FindnonText(const char *input_gpu, int *nontxtList_gpu, int fsiz
 		nontxtList_gpu[idx]=0;
 	}
 
-	__syncthreads();
+	tree[0][idx] = nontxtList_gpu[idx];
+
 }
 
 __device__ int power2(int pow){
@@ -44,26 +47,45 @@ __device__ int power2(int pow){
 	return ans;
 }
 
-__global__ void indexSum(int *indexList, int len, int donetxt){
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-	int val = indexList[donetxt + idx]; // range[threadIdx.x];
-	for(int i=0; i<9; i++){
-		if(((int)threadIdx.x - power2(i)) >= 0){
-			val = val + indexList[donetxt + idx - power2(i)];
-			__syncthreads();
-			indexList[donetxt + idx] = val;
-		}
-	}
+/*
+__global__ void indextreeInit(int *indexList, int text_size){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	tree[0][idx] = indexList[idx];
 }
 
-__device__ int tree[9][40000000];
-__device__ int lock[9];
+__global__ void indextreeSum(int *indexList, int text_size, int base){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	//tree[0][idx] = indexList[idx];
+	//indexList[idx] = 0;
+	
+	if(tree[base][idx] != 0 && idx - power2(base) >= 0){
+		tree[base+1][idx] = tree[base][idx] & tree[base][idx - power2(base)];
+	}
+}
+*/
+/*
+__global__ void treeDown(int *indexList, int base){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int index = idx;
+	indexList[idx] = 0;
+	
+	while(base >= 0 and index >= 0){
+		if(tree[base][index] == 0){
+			base--;
+		}
+		else{
+			indexList[idx] += power2(base);
+			index = index - power2(base);
+		}
+	}
+}*/
 
-__global__ void indextreeSum(int *indexList, int text_size, int nthread){
+
+__global__ void indextreeSum(int *indexList, int text_size){
 
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-//	__device__ int tree[9][1000];
 	
 	tree[0][idx] = indexList[idx];
 	indexList[idx] = 0;
@@ -71,8 +93,6 @@ __global__ void indextreeSum(int *indexList, int text_size, int nthread){
 	int base = 0;
 	while(tree[base][idx] != 0 && idx - power2(base) >= 0){
 		tree[base+1][idx] = tree[base][idx] & tree[base][idx - power2(base)];
-//		atomicAdd(&lock[base], 1);
-//		while(lock[base] != text_size){}
 		base++;
 	}
 	int index = idx;
@@ -85,97 +105,27 @@ __global__ void indextreeSum(int *indexList, int text_size, int nthread){
 			index = index - power2(base);
 		}
 	}
-
-/*
-
-__global__ void indextreeSum( int *indexList, int text_size){
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	//printf("idx = %d ", idx);
-	
-	tree[(0 * text_size) + idx] = indexList[idx];
-	indexList[idx] = 0;
-
-	int base = 0;
-	while(tree[(base * text_size) + idx] != 0 and idx - power2(base) >= 0){
-		tree[((base+1) * text_size) + idx] = tree[(base * text_size) + idx] & tree[(base * text_size) + idx - power2(base)];
-		base++;
-	}
-
-	int index = idx;
-	while(base >= 0 and index >= 0){
-		if(tree[(base * text_size) + index] == 0){
-			base--;
-		}
-		else{
-			indexList[idx] += power2(base);
-			index = index - power2(base);
-		}
-	}
-	*/
-/*
-	base--;
-	while(base >= 0){
-		indexList[idx] += tree[base][idx - ]		
-	}
-
-	indexList[idx] = power2(base);
-	base--;
-	while(base >= 0){
-		indexList[idx] += power2(base) * (tree[base][idx - power2(base-1)] );	
-		base--;
-	}
-*/
 }
+
 
 void CountPosition(const char *text, int *pos, int text_size)
 {
-	/*
-		SyncedMemory<char> text_sync(text.data(), text_gpu, n);
-		text_sync.get_cpu_wo();
-		int *pos_yours_gpu = pos_yours_sync.get_gpu_wo();
-		CountPosition(text_sync.get_gpu_ro(), pos_yours_gpu, n);
-	*/
 
 	FindnonText<<<(text_size/32)+1, 32>>>(text, pos, text_size);
 	
-//	int* device_data = NULL;
-//	cudaMalloc((void**)&device_data, 9 * text_size * sizeof(int*));  
 
-//	printf("%d\n", (text_size/32)+1);	
-	indextreeSum<<<(text_size/32)+1, 32>>>( pos, text_size, (text_size/32)+1);
+//	indextreeInit<<<(text_size/32)+1, 32>>>(pos, text_size);
 
-
-/**	
-	int cpuList[text_size];
-	cudaMemcpy(cpuList, pos, text_size*sizeof(int), cudaMemcpyDeviceToHost);
-
-	int donetxt = 0, len = 0;
-	int *startptr = cpuList;
-	int *endptr = startptr;
-	while(donetxt < text_size){
-
-		len = 0;
-		while(*startptr != 1){startptr++; donetxt++;}
-		endptr = startptr;
-		while(*endptr == 1){endptr++; len++;}
-
-		indexSum<<<1, len>>>(pos, len, donetxt);
-		startptr = endptr;
-		donetxt = donetxt + len;
+	indextreeSum<<<(text_size/32)+1, 32>>>(pos, text_size);
+	
+	/*
+	for(int i=0;i<10;i++){
+		indextreeSum<<<(text_size/32)+1, 32>>>(pos, text_size, i);
 	}
-*/
-/**
-	int count=0;
-	for(int i=0;i<text_size;i++){
-		if(cpuList[i]==0){
-			count=0;
-		}
-		else{
-			count++;
-			cpuList[i]=count;
-		}
-*/
-
+	
+	treeDown<<<(text_size/32)+1, 32>>>(pos, 9);
+	cudaDeviceSynchronize();
+	*/
 
 }
 
