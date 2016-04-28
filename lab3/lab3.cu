9915cd43a12ada6091f5d8a78e0a1b2eab1e86ba
yafangshih <ya.fang.shih.tw@gmail.com>
2016-04-29 02:04:59 +0800

    dont look back in anger

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -1,5 +1,9 @@
 #include "lab3.h"
 #include <cstdio>
+#include <thrust/transform.h>
+#include <thrust/functional.h>
+#include <thrust/device_ptr.h>
+#include <thrust/reduce.h>
 
 __device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
@@ -27,59 +31,7 @@ __global__ void SimpleClone(
 	}
 }
 
-__global__ void findEdge(float* edge, const float* mask, int wt, int ht, const int oy, const int ox, int wb, int hb){
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
-	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;
-	int xoffset, yoffset;
-	int xneibor, yneibor;
-
-	if ((0 <= yt) and (yt < ht) and (0 <= xt) and (xt < wt) ) {
-		int ans = 0;
-		int grad = 0;
-		for(int i=0;i<4;i++){
-			if(i==0) {yoffset = -1; xoffset = 0; }
-			else if(i==1) {yoffset = 0; xoffset = -1; }
-			else if(i==2) {yoffset = 0; xoffset = 1; }
-			else {yoffset = 1; xoffset = 0; }
-
-			yneibor = yt+yoffset;
-			xneibor = xt+xoffset;
-			
-			if( ! ((yneibor >= 0) && (yneibor < ht) && (xneibor >= 0) && (xneibor < wt))){ continue; }
-			else{
-				grad += mask[curt] + (-1)*(mask[yneibor*wt + xneibor]);
-			}
-		}
-		if(grad > 0 || grad < 0){
-			ans = 127;
-		}
-		else if(mask[curt] < 127){ 
-			ans = 0; 
-		}
-		else if(mask[curt] > 127){ ans = 255;}
-		edge[curt] = ans;
-	}
-
-}
-/*
-__global__ void toEdgepad(float* padmask, float* edge, int wt, int ht){
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
-	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;
-
-	if ((0 < yt) and (yt < ht-1) and (0 < xt) and (xt < wt-1)) {
-		const int cure = (wt-2)*(yt-1)+(xt-1);
-		padmask[curt] = edge[cure];
-	}	
-	else if((yt < ht) and (xt < wt)){
-		padmask[curt] = -1.0;
-	}
-
-}
-*/
-__global__ void computeAb(const float* edge, const float* background, float* A, float* b, float* x, const int ht, const int wt, const int c, int *cntgpu, int *f){
-//(edge, background, A, b, ht, wt, c, cntgpu)
+__global__ void computeAb(const float* mask, const float* background, const float* target, float* A, float* b, float* x, const int ht, const int wt, const int oy, const int ox, const int wb, const int c, int *f){
 
 	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
 	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
@@ -91,68 +43,52 @@ __global__ void computeAb(const float* edge, const float* background, float* A, 
 
 	if ((0 <= yt) and (yt < ht) and (0 <= xt) and (xt < wt) ) {
 
-	//	atomicAdd(cntgpu, 1);
+		if(mask[curt] > 127){
+			x[curt] = 0;
 
-		for(int i=0;i<4;i++){
-			if(i==0) {yoffset = -1; xoffset = 0; }
-			else if(i==1) {yoffset = 0; xoffset = -1; }
-			else if(i==2) {yoffset = 0; xoffset = 1; }
-			else {yoffset = 1; xoffset = 0; }
+			for(int i=0;i<4;i++){
+				if(i==0) {yoffset = -1; xoffset = 0; }
+				else if(i==1) {yoffset = 0; xoffset = -1; }
+				else if(i==2) {yoffset = 0; xoffset = 1; }
+				else {yoffset = 1; xoffset = 0; }
 
-			yneibor = yt+yoffset;
-			xneibor = xt+xoffset;
-		
-			if( ! ((yneibor >= 0) && (yneibor < ht) && (xneibor >= 0) && (xneibor < wt))){ 
-				atomicAdd(cntgpu, 1);
-				continue; 
-			}
-			else{
-	
-				int neibor = yneibor*wt + xneibor;
-
-				if(edge[neibor] < 64){ 
-
-				} //black, background
-				else if ((191 > edge[neibor]) && (edge[neibor] > 64) ){ 
-					b[curt] += background[(neibor)*3 + c]; 
-					count+=1;
-				} 
-				else if (edge[neibor] > 191){ 
+				yneibor = yt+yoffset;
+				xneibor = xt+xoffset;
+			
+				if( ! ((yneibor >= 0) && (yneibor < ht) && (xneibor >= 0) && (xneibor < wt))){ 
+					continue; 
+				}
+				else{
+					int neibor = yneibor*wt + xneibor;
+					count ++;
 					pos = curt*5 + i + 1;
 					A[pos] = -1;
-					count = count + 1;
-				} 
+					b[curt] += (target[curt*3 + c] - target[(neibor)*3 + c]);
+				}
+				A[curt*5] = count;
 			}
-	
 		}
-//		pos = (curt)*(wt*ht) + curt;
-		A[curt*5] = count;
-//		printf("%f %f %f %f %f\n", A[curt*5], A[curt*5+1], A[curt*5+2], A[curt*5+3], A[curt*5+4]);
-		
+		else{
+			x[curt] = background[((yt+oy)*wb + (xt+ox))*3 + c];
+//			A[curt*5] = 1;
+			for(int i=0; i<5; i++) { A[curt*5 + i] = 0; }
+			b[curt] = background[((yt+oy)*wb + (xt+ox))*3 + c]; //0;
+		}
 	}
 
 
-/*
-	if (0 < yt and yt < ht-1 and 0 < xt and xt < wt-1) {
-		if(mask[(yt-1)*wt + xt] < 64){  } //black, background
-		else if ((191 > mask[(yt-1)*wt + xt]) and mask[(yt-1)*wt + xt] > 64 ){ b[] } //edge
-		else if (mask[(yt-1)*wt + xt] > 191){ A[((yt-1)*(wt-2) + xt-1)*(wt-2)*(ht-2) + (yt-2)*(wt-2) + xt-1] = -1; } //white
-	}	
-*/
 }
 
-__global__ void computeb(float* b, const float* target, const float* edge, const float* background, float* x, int ht, int wt, int c, int *cntgpu){
-	//(b, target, edge, ht, wt, c)
 
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
-	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;
-	int yoffset, xoffset, yneibor, xneibor;
+__global__ void jacobiRow(float* x, float* tmpx, const float* A, const float* b, int ht, int wt){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	float sum = 0;
 
+	int yt = idx/wt, xt = idx%wt;
+	int yoffset, xoffset, yneibor, xneibor, neibor;
 
-	if ((0 <= yt) and (yt < ht) and (0 <= xt) and (xt < wt) ) {
-
-		if(edge[curt] < 191){x[curt] = background[curt*3 +c];}
+	if(idx < ht*wt){
+//		sum = sum+ A[idx*5] * x[idx];
 
 		for(int i=0;i<4;i++){
 			if(i==0) {yoffset = -1; xoffset = 0; }
@@ -163,60 +99,31 @@ __global__ void computeb(float* b, const float* target, const float* edge, const
 			yneibor = yt + yoffset;
 			xneibor = xt + xoffset;
 
-			
 			if( ! ((yneibor >= 0) && (yneibor < ht) && (xneibor >= 0) && (xneibor < wt))){ 
-				atomicAdd(cntgpu, 1);
 				continue; 
 			}
 
-			int neibor = yneibor*wt + xneibor;
-			if(edge[neibor] > 64){ // not background
-				b[curt] += (target[curt*3 + c] - target[(neibor)*3 + c]);
-			}
+			neibor = yneibor*wt + xneibor;
+			sum = sum+ A[idx*5 +i +1] * x[neibor];
+		}
+		tmpx[idx] = sum;
+	}
+}
+
+__global__ void copy2x(float* x, float* tmpx, float *A, float *b, const float * mask, int ht, int wt, int * f){
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+//	prnt[idx] = idx;
+
+	if(idx < ht*wt){
+		if(mask[idx] > 127){
+		//x[idx] = b[idx] - tmpx[idx];
+			x[idx] = (b[idx] - tmpx[idx])/A[idx*5];
+			if(b[idx] - (x[idx]*A[idx*5] + tmpx[idx]) < 50){f[idx] = 0;}
 		}
 	}
 }
 
-
-__global__ void jacobiRow(float* x, float* tmpx, const float* A, const float* b, int ht, int wt){
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	float sum = 0;
-
-	int yt = idx/wt, xt = idx%wt;
-//	printf("%d %d\n", yt, xt);
-
-	int yoffset, xoffset, yneibor, xneibor, neibor;
-
-	sum = sum+ A[idx*5] * x[idx];
-
-	for(int i=0;i<4;i++){
-		if(i==0) {yoffset = -1; xoffset = 0; }
-		else if(i==1) {yoffset = 0; xoffset = -1; }
-		else if(i==2) {yoffset = 0; xoffset = 1; }
-		else {yoffset = 1; xoffset = 0; }
-
-		yneibor = yt + yoffset;
-		xneibor = xt + xoffset;
-
-		if( ! ((yneibor >= 0) && (yneibor < ht) && (xneibor >= 0) && (xneibor < wt))){ 
-			continue; 
-		}
-
-		neibor = yneibor*wt + xneibor;
-		sum = sum+ A[idx*5 +i +1] * x[neibor];
-	}
-
-	tmpx[idx] = sum;
-}
-
-__global__ void copy2x(float* x, float* tmpx, float *b, int* prnt){
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	prnt[idx] = idx;
-	x[idx] = b[idx] - tmpx[idx];
-
-}
-
-__global__ void paste(float* output, float* x, int wt, int ht, const int oy, const int ox, int wb, int hb, int c){
+__global__ void paste(float* output, float* x, const float* background, int wt, int ht, const int oy, const int ox, int wb, int hb, int c){
 	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
 	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
 	const int curt = wt*yt+xt;
@@ -229,38 +136,14 @@ __global__ void paste(float* output, float* x, int wt, int ht, const int oy, con
 		}
 	}
 }
-/*
-__global__ void show(float* output, float* padmask, int wt, int ht, const int oy, const int ox, int wb, int hb){
-	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
-	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
-	const int curt = wt*yt+xt;
 
-	if (yt < ht and xt < wt) {
-		const int yb = oy+yt, xb = ox+xt;
-		const int curb = wb*yb+xb;
-		if (0 <= yb and yb < hb and 0 <= xb and xb < wb) {
-			output[curb*3+0] = padmask[curt];
-			output[curb*3+1] = padmask[curt];
-			output[curb*3+2] = padmask[curt];
-		}
+int checkdone(int* fcpu, int ht, int wt){
+	for(int i=0; i<wt*ht; i++){
+		if(fcpu[i] == 0){return 1;}
 	}
+	return 0;
 }
-*/
-/*
-void addpad(float* padmask, const float* mask, int wt, int ht){
-	cudaMemcpy(padmask, mask, sizeof(float), cudaMemcpyDeviceToDevice);
-	cudaMemcpy(padmask+1, mask, wt*sizeof(float), cudaMemcpyDeviceToDevice);
-	cudaMemcpy(padmask+wt+1, mask+wt-1, sizeof(float), cudaMemcpyDeviceToDevice);
-	for(int i=1, j=0; i<ht+1; i++, j++){
-		cudaMemcpy(padmask + i*(wt+2), mask + j*wt, sizeof(float), cudaMemcpyDeviceToDevice);
-		cudaMemcpy(padmask + i*(wt+2) +1, mask + j*wt, wt*sizeof(float), cudaMemcpyDeviceToDevice);
-		cudaMemcpy(padmask + (i+1)*(wt+2) -1, mask + (j+1)*wt - 1, sizeof(float), cudaMemcpyDeviceToDevice);
-	}
-	cudaMemcpy(padmask + (ht+1) *(wt+2), mask + (ht-1)*wt, sizeof(float), cudaMemcpyDeviceToDevice);
-	cudaMemcpy(padmask + (ht+1)*(wt+2) +1, mask + (ht-1)*wt, wt*sizeof(float), cudaMemcpyDeviceToDevice);
-	cudaMemcpy(padmask + (ht+1+1)*(wt+2) -1, mask + (ht-1+1)*wt - 1, sizeof(float), cudaMemcpyDeviceToDevice);
-}
-*/
+
 void PoissonImageCloning(
 	const float *background,
 	const float *target,
@@ -271,53 +154,23 @@ void PoissonImageCloning(
 )
 {
 	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
-/*	SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
-		background, target, mask, output,
-		wb, hb, wt, ht, oy, ox
-	);
-*/
 
-/*	// padding
-	float *padmask;
-	cudaMalloc((void **) &padmask, (wt+2)*(ht+2)*sizeof(float));
-	addpad(padmask, mask, wt, ht);
-*/
 
 	int *f;
-	cudaMalloc((void **) &f, sizeof(int));
-	cudaMemset((void*)f, 0, sizeof(int));
-	int *fcpu = (int *)malloc(sizeof(int));
+	cudaMalloc((void **) &f, wt*ht*sizeof(int));
+	cudaMemset((void*)f, 0, wt*ht*sizeof(int));
+	int *fcpu = (int *)malloc(wt*ht*sizeof(int));
 
-	// find gradient edges
-	float *edge;
-	cudaMalloc((void **) &edge, wt*ht*sizeof(float));
+	thrust::device_ptr<int> flag_d(f);
+
 
 	float *tmpx;
 	cudaMalloc((void **) &tmpx, wt*ht*sizeof(float));
 	cudaMemset((void*)tmpx, 0, wt*ht*sizeof(float));
 
-	findEdge<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(edge, mask, wt, ht, oy, ox, wb, hb);
-
-/*
-	cudaMemcpy(fcpu, f, sizeof(int), cudaMemcpyDeviceToHost);
-	printf("f = %d \n",*fcpu);
-	cudaMemset((void*)f, 0, sizeof(int));
-*/
-	/*
-	for(int c=0;c<3;c++)
-		paste<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(output, edge, wt, ht, oy, ox, wb, hb, c);
-*/
-//	toEdgepad<<<dim3(CeilDiv(wt+2,32), CeilDiv(ht+2,16)), dim3(32,16)>>>(padmask, edge, wt+2, ht+2);
-/*
-	for(int c=0;c<3;c++){
-		paste<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(output, edge, wt, ht, oy, ox, wb, hb, c);
-	}
-*/
 
 
 	float *A;
-//	int size = ((wt*ht)/4)*(wt*ht);
-//	printf("size = %d\n", size);
 	cudaMalloc((void **) &A, 5*(wt*ht)*sizeof(float));
 	cudaMemset((void*)A, 0, 5*(wt*ht)*sizeof(float));
 
@@ -326,71 +179,35 @@ void PoissonImageCloning(
 	float *x;
 	cudaMalloc((void **) &x, wt*ht*sizeof(float));
 
+	int notyet = 0;
 
-
-	int *cntgpu;
-	cudaMalloc((void **) &cntgpu, sizeof(int));
-	cudaMemset((void*)cntgpu, 0, sizeof(int));
-	int *cnt = (int *)malloc(sizeof(int));
-/*
-	int *f;
-	cudaMalloc((void **) &f, sizeof(int));
-	cudaMemset((void*)f, 0, sizeof(int));
-	int *fcpu = (int *)malloc(sizeof(int));
-*/
-
-	int *prntcpu = (int *)malloc((wt*ht)*sizeof(int));
-	int *prnt;
-	cudaMalloc((void **) &prnt, (wt*ht)*sizeof(int));
 
 	for(int c=0; c<3; c++){
+		cudaMemset((void*)A, 0, 5*(wt*ht)*sizeof(float));
 		cudaMemset((void*)b, 0, wt*ht*sizeof(float));
 		cudaMemset((void*)x, 0, wt*ht*sizeof(float));
+		cudaMemset((void*)tmpx, 0, wt*ht*sizeof(float));
+		cudaMemset((void*)f, 1, wt*ht*sizeof(int));
+		memset((void*)fcpu, 0,  wt*ht*sizeof(int));
 
-	//	cudaMemset((void*)cntgpu, 0, sizeof(int));
+		notyet = thrust::reduce(flag_d, flag_d + wt*ht);
+		printf("%d %d\n", c, notyet);
 
-		computeAb<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(edge, background, A, b, tmpx, ht, wt, c, cntgpu, f);
-		
-		cudaMemcpy(cnt, cntgpu, sizeof(int), cudaMemcpyDeviceToHost);
-		printf("cnt = %d \n",*cnt);
-		cudaMemset((void*)cntgpu, 0, sizeof(int));
-/*
-		cudaMemcpy(fcpu, f, sizeof(int), cudaMemcpyDeviceToHost);
-		printf("f = %d \n",*fcpu);
-		cudaMemset((void*)f, 0, sizeof(int));
-*/
-		computeb<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(b, target, edge, background, tmpx, ht, wt, c, cntgpu);
-	//	cudaMemcpy(prnt, b, (wt*ht)*sizeof(float), cudaMemcpyDeviceToHost);
-	//	for(int j=0;j<wt*ht; j++){ printf("%f ", prnt[j]);}
-
-
-	//	cudaMemcpy(cnt, cntgpu, sizeof(int), cudaMemcpyDeviceToHost);
-		//for(int j=0;j<wt*ht; j++){if(bpnt[j] > 0.0) printf("%f ", bpnt[j]);}
-	//	printf("cnt = %d \n",*cnt);
-
+		computeAb<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(mask, background, target, A, b, x, ht, wt, oy, ox, wb, c, f);
 	
-		for(int i=0;i<3;i++){
-//			cudaMemset((void*)tmpx, 0, wt*ht*sizeof(float));
+	//	int iter =0;
+	//	while(checkdone(fcpu, ht, wt)){
+		for(int i=0;i<80000;i++){
 			jacobiRow<<<((ht*wt)/32)+1, 32>>>(x, tmpx, A, b, ht, wt);
-/*
-			if(i == 9){
-				cudaMemcpy(prnt, tmpx, (wt*ht)*sizeof(float), cudaMemcpyDeviceToHost);
-				for(int j=0;j<wt*ht; j++){ printf("%f ", prnt[j]);}
-			}
-*/
-			copy2x<<<((ht*wt)/32)+1, 32>>>(x, tmpx, b, prnt);
-//			cudaMemcpy(prntcpu, prnt, (wt*ht)*sizeof(int), cudaMemcpyDeviceToHost);
-//			for(int j=0;j<wt*ht; j++){ printf("%d ", prntcpu[j]);}
+			copy2x<<<((ht*wt)/32)+1, 32>>>(x, tmpx, A, b, mask, ht, wt, f);
+
+//			notyet = thrust::reduce(flag_d, flag_d + wt*ht);
+//			printf("%d %d\n", c, notyet);
+//			cudaMemcpy(fcpu, f, wt*ht*sizeof(int), cudaMemcpyDeviceToHost);
 		}
 
-		paste<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(output, x, wt, ht, oy, ox, wb, hb, c);
+		paste<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(output, x, background, wt, ht, oy, ox, wb, hb, c);
 		
-//		cudaMemcpy(prnt, x, (wt*ht)*sizeof(float), cudaMemcpyDeviceToHost);
-//		for(int j=0;j<wt*ht; j++){ printf("%f ", prnt[j]);}
 	}
-	
-	//show<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(output, edge, wt, ht, oy, ox, wb, hb);
-
-
 
 }
