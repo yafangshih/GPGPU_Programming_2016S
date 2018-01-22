917f59cfe7cd38ad21488e7f01dfa0af1c58e296
yafangshih <avanshih@gmail.com>
2018-01-22 22:59:25 +0800

    Add lab3 README images & Remove legacy codes

--- lab2/main.cu
+++ lab2/main.cu
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstdint>
 #include <cstdlib>
-#include "SyncedMemory.h"
+#include "../utils/SyncedMemory.h"
 #include "lab2.h"
 using namespace std;
 
--- lab3/mkrun.sh
+++
@@ -1,1 +0,0 @@
-./main lab3_test/img_background.ppm lab3_test/img_target.ppm lab3_test/img_mask.pgm 130 600 output.ppm
